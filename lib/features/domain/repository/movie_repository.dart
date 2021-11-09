70533e8b437f83af446db4a29ebc51a0e0ebec27
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-11-09 11:45:08 -0300

    refactoring MovieDetailsRepository

--- lib/features/domain/repository/movie_repository.dart
+++ lib/features/domain/repository/movie_repository.dart
@@ -1,7 +1,8 @@
 import 'package:app_movie/core/error/failure.dart';
+import 'package:app_movie/core/utils/parameters.dart';
 import 'package:app_movie/features/domain/entities/movie.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class MovieDetailsRepository {
-  Future<Either<Failure, Movie>> getmovieDetails();
+  Future<Either<Failure, Movie>> getmovieDetails(Parameters parameters);
 }
