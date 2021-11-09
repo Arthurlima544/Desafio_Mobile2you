db29994bba3cafb760d19740de8d439e4550a621
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-11-09 10:57:44 -0300

    MovieDetailsRepository and Failure Class Created

---
+++ lib/features/domain/repository/movie_repository.dart
@@ -0,0 +1,7 @@
+import 'package:app_movie/core/error/failure.dart';
+import 'package:app_movie/features/domain/entities/movie.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class MovieDetailsRepository {
+  Future<Either<Failure, Movie>> getmovieDetails();
+}
