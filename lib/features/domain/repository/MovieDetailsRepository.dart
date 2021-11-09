5534a691fc3626a5d74b426c27115aaa119ed923
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-11-09 16:24:48 -0300

    adding http package

--- lib/features/domain/repository/MovieDetailsRepository.dart
+++ lib/features/domain/repository/MovieDetailsRepository.dart
@@ -1,4 +1,4 @@
-import 'package:app_movie/core/error/failure.dart';
+import 'package:app_movie/core/error/Failures.dart/failure.dart';
 import 'package:app_movie/core/utils/parameters.dart';
 import 'package:app_movie/features/domain/entities/movie.dart';
 import 'package:dartz/dartz.dart';
