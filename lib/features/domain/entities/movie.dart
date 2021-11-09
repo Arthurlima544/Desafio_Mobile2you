9f3f1a66889ee7d97748d8c4320542a4afbde2a1
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-11-09 10:45:46 -0300

    Movie Detail Entity Created and new package Added Equatable

---
+++ lib/features/domain/entities/movie.dart
@@ -0,0 +1,25 @@
+import 'dart:ffi';
+
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final int id;
+  final String title;
+  final int likes;
+  final Float popularity;
+
+  Movie({
+    required this.id,
+    required this.title,
+    required this.likes,
+    required this.popularity,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        likes,
+        popularity,
+      ];
+}
