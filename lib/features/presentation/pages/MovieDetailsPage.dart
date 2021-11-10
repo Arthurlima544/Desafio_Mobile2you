c4ddf79212459c74395c4a7cfa54cdbe8d172315
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-11-10 14:25:57 -0300

    adding moviedetails page

---
+++ lib/features/presentation/pages/MovieDetailsPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Center(
+            child: Text(
+              "teste",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 12,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
