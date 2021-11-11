c148225d9c59e851acffbc82bbefd2c8ae760ba9
Arthurlima544 <arthurlindaolima544@gmail.com>
2021-11-11 18:37:27 -0300

    adding usecase for SimilarMovies

---
+++ lib/features/data/datasource/SimilarMovieDataSourceImpl.dart
@@ -0,0 +1,11 @@
+import 'package:app_movie/features/data/models/similarMoviesModel.dart';
+
+import 'SimilarMovieDataSource.dart';
+
+class SimilarMovieDataSourceImpl implements SimilarMovieDataSource {
+  @override
+  Future<SimilarMoviesModel> getSimilarMovies(int id, int page) {
+    // TODO: implement getSimilarMovies
+    throw UnimplementedError();
+  }
+}
